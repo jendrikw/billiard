4de90bf5d86a24060a562cae2c38792760584814
Jendrik Wenke <buhtig@jendrik.eu>
2018-09-25 11:05:22 +0200

    add example js with classes

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-alert('hallo');
+'use strict';
+
+class Kugel {
+	constructor() {
+		this.x = 0
+		this.y = 0
+	}
+	method() {
+		return '(' + this.x + ', ' + this.y + ')';
+	}
+}
+
+var eineVariable = new Kugel();
+eineVariable.x = 10;
+alert(eineVariable.method());

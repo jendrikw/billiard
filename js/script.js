7a80b4169975726adfa1c5d9bde4d25f67a3f473
Jendrik <buhtig@jendrik.eu>
2018-10-02 10:25:34 +0200

    create prototype table

--- js/script.js
+++ js/script.js
@@ -1,27 +1,52 @@
 'use strict';
 
-class Kugel {
-	constructor() {
-		this.x = 0;
-		this.y = 0;
-	}
-	method() {
-		return '(' + this.x + ', ' + this.y + ')';
-	}
-	draw(ctx) {
-		ctx.beginPath();
+class Table {
+
+    static get WIDTH() {
+        return 600;
+    }
+
+    static get HEIGHT() {
+        return 300;
+    }
+
+    constructor() {
+    }
+
+    draw(ctx) {
+        ctx.beginPath();
+        ctx.strokeStyle = "green";
+        let x = (ctx.canvas.width - Table.WIDTH) / 2;
+        let y = (ctx.canvas.height - Table.HEIGHT) / 2;
+        ctx.strokeRect(x, y, Table.WIDTH, Table.HEIGHT);
+        ctx.closePath();
+    }
+}
+
+
+class Ball {
+    constructor(x, y, color) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+
+    draw(ctx) {
+        ctx.beginPath();
         ctx.arc(this.x, this.y, 20, 0, 2 * Math.PI);
-        ctx.fillStyle = "red";
+        ctx.fillStyle = this.color;
         ctx.fill();
-		ctx.closePath();
-	}
+        ctx.closePath();
+    }
 }
 
 function onload() {
-    let eineVariable = new Kugel();
-    eineVariable.x = 100;
-    eineVariable.y = 100;
     let canvas = document.getElementById("canvas");
     let ctx = canvas.getContext("2d");
-    eineVariable.draw(ctx);
+    let ball1 = new Ball(100, 100, "red");
+    ball1.x = 100;
+    ball1.y = 100;
+    ball1.draw(ctx);
+    let table = new Table();
+    table.draw(ctx);
 }

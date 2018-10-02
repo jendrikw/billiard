64079c4ceb29b71cdaca2c0ad71255e46d44019b
Jendrik <buhtig@jendrik.eu>
2018-10-02 17:44:48 +0200

    multiple balls

--- js/script.js
+++ js/script.js
@@ -30,6 +30,11 @@ class Table {
 
 
 class Ball {
+
+    static get RADIUS() {
+        return scaleRealCmToPx(6.15) / 2;
+    }
+
     constructor(x, y, color) {
         this.x = x;
         this.y = y;
@@ -38,18 +43,48 @@ class Ball {
 
     draw(ctx) {
         ctx.beginPath();
-        ctx.arc(this.x, this.y, scaleRealCmToPx(6.15)/2, 0, 2 * Math.PI);
+        ctx.arc(this.x, this.y, Ball.RADIUS, 0, 2 * Math.PI);
         ctx.fillStyle = this.color;
         ctx.fill();
         ctx.closePath();
     }
+
+    contains(x, y) {
+        let dx = x - this.x;
+        let dy = y - this.y;
+        return Math.sqrt(dx * dx + dy * dy) <= Ball.RADIUS;
+    }
+
+    moveTo(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Game {
+    constructor(ctx) {
+        this.ctx = ctx;
+        this.table = new Table();
+        this.balls = [];
+        for (let i = 0; i <= 15; i++) {
+            this.balls[i] = new Ball(100 + 20 * i,  200, "red");
+        }
+    }
+
+    drawAll() {
+        this.table.draw(this.ctx);
+        for (let b of this.balls) {
+            b.draw(this.ctx);
+        }
+    }
+
 }
 
 function onload() {
     let canvas = document.getElementById("canvas");
     let ctx = canvas.getContext("2d");
-    let table = new Table();
-    table.draw(ctx);
-    let ball1 = new Ball(200, 200, "red");
-    ball1.draw(ctx);
+
+    let game = new Game(ctx);
+    game.drawAll();
+
 }

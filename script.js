d8a9a51a422fe86b78a07e0398bba5f822185c0e
Jendrik <buhtig@jendrik.eu>
2018-10-02 09:30:02 +0200

    add canvas and draw example ball

--- script.js
+++ script.js
@@ -2,14 +2,26 @@
 
 class Kugel {
 	constructor() {
-		this.x = 0
-		this.y = 0
+		this.x = 0;
+		this.y = 0;
 	}
 	method() {
 		return '(' + this.x + ', ' + this.y + ')';
 	}
+	draw(ctx) {
+		ctx.beginPath();
+        ctx.arc(this.x, this.y, 20, 0, 2 * Math.PI);
+        ctx.fillStyle = "red";
+        ctx.fill();
+		ctx.closePath();
+	}
 }
 
-var eineVariable = new Kugel();
-eineVariable.x = 10;
-alert(eineVariable.method());
+function onload() {
+    let eineVariable = new Kugel();
+    eineVariable.x = 100;
+    eineVariable.y = 100;
+    let canvas = document.getElementById("canvas");
+    let ctx = canvas.getContext("2d");
+    eineVariable.draw(ctx);
+}

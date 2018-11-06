d011a471249c19f8d0f4202194315533d0681fcd
Jendrik <buhtig@jendrik.eu>
2018-11-06 08:35:01 +0100

    add holes

--- js/script.js
+++ js/script.js
@@ -12,96 +12,188 @@ class Table {
     }
 
     draw() {
-        const canvasWidth = this.context.canvas.width;
-        let xLeft = (canvasWidth - Table.WIDTH) / 2;
-        let xRight = xLeft + Table.WIDTH;
-        let yTop = (this.context.canvas.height - Table.HEIGHT) / 2;
-        let yBottom = yTop + Table.HEIGHT;
-        const borderWidth = 20;
+        const xLeft = (this.context.canvas.width - Table.WIDTH) / 2;
+        const xMiddle = xLeft + Table.WIDTH / 2;
+        const xRight = xLeft + Table.WIDTH;
 
-        this.context.beginPath();
-		this.context.fillStyle = "green";
-		this.context.fillRect(xLeft, yTop, Table.WIDTH, Table.HEIGHT);
+        const yTop = (this.context.canvas.height - Table.HEIGHT) / 2;
+        const yBottom = yTop + Table.HEIGHT;
+        const holeRadius = 1.2 * Ball.RADIUS;
+        const holeOffset = Math.sqrt(holeRadius);
 
 		// top
-		const topGradient = this.context.createLinearGradient(0, yTop - borderWidth, 0, yTop);
-        topGradient.addColorStop(0, "#432918");
-        topGradient.addColorStop(1, "#7d4e24");
+		const topGradient = this.context.createLinearGradient(0, yTop - Table.BORDER_WIDTH, 0, yTop);
+        topGradient.addColorStop(0, Table.BORDER_OUTER_COLOR);
+        topGradient.addColorStop(1, Table.BORDER_INNER_COLOR);
         this.context.fillStyle = topGradient;
-        this.context.fillRect(xLeft, yTop - borderWidth, Table.WIDTH, borderWidth + 1); // 1 for antialiasing
+        this.context.fillRect(xLeft, yTop - Table.BORDER_WIDTH, Table.WIDTH, Table.BORDER_WIDTH + 1); // 1 for antialiasing
 
         // right
-        const rightGradient = this.context.createLinearGradient(xRight + borderWidth, 0, xRight, 0);
-        rightGradient.addColorStop(0, "#432918");
-        rightGradient.addColorStop(1, "#7d4e24");
+        const rightGradient = this.context.createLinearGradient(xRight + Table.BORDER_WIDTH, 0, xRight, 0);
+        rightGradient.addColorStop(0, Table.BORDER_OUTER_COLOR);
+        rightGradient.addColorStop(1, Table.BORDER_INNER_COLOR);
         this.context.fillStyle = rightGradient;
-        this.context.fillRect(xRight, yTop, borderWidth, Table.HEIGHT);
+        this.context.fillRect(xRight, yTop, Table.BORDER_WIDTH, Table.HEIGHT);
 
         // bottom
-        const bottomGradient = this.context.createLinearGradient(0, yBottom + borderWidth, 0, yBottom);
-        bottomGradient.addColorStop(0, "#432918");
-        bottomGradient.addColorStop(1, "#7d4e24");
+        const bottomGradient = this.context.createLinearGradient(0, yBottom + Table.BORDER_WIDTH, 0, yBottom);
+        bottomGradient.addColorStop(0, Table.BORDER_OUTER_COLOR);
+        bottomGradient.addColorStop(1, Table.BORDER_INNER_COLOR);
         this.context.fillStyle = bottomGradient;
-        this.context.fillRect(xLeft, yBottom - 1, Table.WIDTH, borderWidth + 1); // 1 for antialiasing
+        this.context.fillRect(xLeft, yBottom - 1, Table.WIDTH, Table.BORDER_WIDTH + 1); // 1 for antialiasing
 
         // left
-        const leftGradient = this.context.createLinearGradient(xLeft - borderWidth, 0, xLeft, 0);
-        leftGradient.addColorStop(0, "#432918");
-        leftGradient.addColorStop(1, "#7d4e24");
+        const leftGradient = this.context.createLinearGradient(xLeft - Table.BORDER_WIDTH, 0, xLeft, 0);
+        leftGradient.addColorStop(0, Table.BORDER_OUTER_COLOR);
+        leftGradient.addColorStop(1, Table.BORDER_INNER_COLOR);
         this.context.fillStyle = leftGradient;
-        this.context.fillRect(xLeft - borderWidth, yTop, borderWidth, Table.HEIGHT);
+        this.context.fillRect(xLeft - Table.BORDER_WIDTH, yTop, Table.BORDER_WIDTH, Table.HEIGHT);
 
         // top left corner
-        const topLeftCornerGradient = this.context.createRadialGradient(xLeft, yTop, borderWidth, xLeft, yTop, 0);
-        topLeftCornerGradient.addColorStop(0.1, "#432918");
-        topLeftCornerGradient.addColorStop(0.9, "#7d4e24");
+        const topLeftCornerGradient = this.context.createRadialGradient(xLeft, yTop, Table.BORDER_WIDTH, xLeft, yTop, 0);
+        topLeftCornerGradient.addColorStop(0.1, Table.BORDER_OUTER_COLOR);
+        topLeftCornerGradient.addColorStop(0.9, Table.BORDER_INNER_COLOR);
         this.context.fillStyle = topLeftCornerGradient;
         this.context.moveTo(xLeft, yTop + 1);
-        this.context.lineTo(xLeft - borderWidth, yTop + 1);
-        this.context.arcTo(xLeft - borderWidth, yTop - borderWidth, xLeft, yTop - borderWidth, borderWidth);
+        this.context.lineTo(xLeft - Table.BORDER_WIDTH, yTop + 1);
+        this.context.arcTo(xLeft - Table.BORDER_WIDTH, yTop - Table.BORDER_WIDTH, xLeft, yTop - Table.BORDER_WIDTH, Table.BORDER_WIDTH);
         this.context.lineTo(xLeft, yTop);
         this.context.fill();
 
         // top right corner
-        const topRightCornerGradient = this.context.createRadialGradient(xRight, yTop, borderWidth, xRight, yTop, 0);
-        topRightCornerGradient.addColorStop(0.1, "#432918");
-        topRightCornerGradient.addColorStop(0.9, "#7d4e24");
+        const topRightCornerGradient = this.context.createRadialGradient(xRight, yTop, Table.BORDER_WIDTH, xRight, yTop, 0);
+        topRightCornerGradient.addColorStop(0.1, Table.BORDER_OUTER_COLOR);
+        topRightCornerGradient.addColorStop(0.9, Table.BORDER_INNER_COLOR);
         this.context.fillStyle = topRightCornerGradient;
         this.context.beginPath();
         this.context.moveTo(xRight, yTop + 1);
-        this.context.lineTo(xRight + borderWidth, yTop + 1);
-        this.context.arcTo(xRight + borderWidth, yTop - borderWidth, xRight, yTop - borderWidth, borderWidth);
+        this.context.lineTo(xRight + Table.BORDER_WIDTH, yTop + 1);
+        this.context.arcTo(xRight + Table.BORDER_WIDTH, yTop - Table.BORDER_WIDTH, xRight, yTop - Table.BORDER_WIDTH, Table.BORDER_WIDTH);
         this.context.lineTo(xRight, yTop);
         this.context.fill();
 
         // bottom right corner
-        const bottomRightCornerGradient = this.context.createRadialGradient(xRight, yBottom, borderWidth, xRight, yBottom, 0);
-        bottomRightCornerGradient.addColorStop(0.1, "#432918");
-        bottomRightCornerGradient.addColorStop(0.9, "#7d4e24");
+        const bottomRightCornerGradient = this.context.createRadialGradient(xRight, yBottom, Table.BORDER_WIDTH, xRight, yBottom, 0);
+        bottomRightCornerGradient.addColorStop(0.1, Table.BORDER_OUTER_COLOR);
+        bottomRightCornerGradient.addColorStop(0.9, Table.BORDER_INNER_COLOR);
         this.context.fillStyle = bottomRightCornerGradient;
         this.context.beginPath();
         this.context.moveTo(xRight, yBottom - 1);
-        this.context.lineTo(xRight + borderWidth, yBottom - 1);
-        this.context.arcTo(xRight + borderWidth, yBottom + borderWidth, xRight, yBottom + borderWidth, borderWidth);
+        this.context.lineTo(xRight + Table.BORDER_WIDTH, yBottom - 1);
+        this.context.arcTo(xRight + Table.BORDER_WIDTH, yBottom + Table.BORDER_WIDTH, xRight, yBottom + Table.BORDER_WIDTH, Table.BORDER_WIDTH);
         this.context.lineTo(xRight, yBottom - 1);
         this.context.fill();
 
         // bottom left corner
-        const bottomLeftCornerGradient = this.context.createRadialGradient(xLeft, yBottom, borderWidth, xLeft, yBottom, 0);
-        bottomLeftCornerGradient.addColorStop(0.1, "#432918");
-        bottomLeftCornerGradient.addColorStop(0.9, "#7d4e24");
+        const bottomLeftCornerGradient = this.context.createRadialGradient(xLeft, yBottom, Table.BORDER_WIDTH, xLeft, yBottom, 0);
+        bottomLeftCornerGradient.addColorStop(0.1, Table.BORDER_OUTER_COLOR);
+        bottomLeftCornerGradient.addColorStop(0.9, Table.BORDER_INNER_COLOR);
         this.context.fillStyle = bottomLeftCornerGradient;
         this.context.beginPath();
         this.context.moveTo(xLeft, yBottom - 1);
-        this.context.lineTo(xLeft - borderWidth, yBottom - 1);
-        this.context.arcTo(xLeft - borderWidth, yBottom + borderWidth, xLeft, yBottom + borderWidth, borderWidth);
+        this.context.lineTo(xLeft - Table.BORDER_WIDTH, yBottom - 1);
+        this.context.arcTo(xLeft - Table.BORDER_WIDTH, yBottom + Table.BORDER_WIDTH, xLeft, yBottom + Table.BORDER_WIDTH, Table.BORDER_WIDTH);
         this.context.lineTo(xLeft, yBottom - 1);
+        this.context.fill();
+
+        // table
+        this.context.beginPath();
+        this.context.fillStyle = Table.COLOR;
+        this.context.fillRect(xLeft, yTop, Table.WIDTH, Table.HEIGHT);
+
+        // top left hole
+        this.context.strokeStyle = Table.COLOR;
+        this.context.lineCap = "round";
+        this.context.lineWidth = 1.9 * holeRadius;
+        this.context.beginPath();
+        this.context.moveTo(xLeft + holeOffset, yTop + holeOffset);
+        this.context.lineTo(xLeft - holeOffset, yTop - holeOffset);
+        this.context.stroke();
+
+        this.context.fillStyle = "black";
+        this.context.beginPath();
+        this.context.arc(xLeft - holeOffset, yTop - holeOffset, holeRadius, 0, 2 * Math.PI);
+        this.context.fill();
+
+        // top middle hole
+        this.context.strokeStyle = Table.COLOR;
+        this.context.lineCap = "round";
+        this.context.lineWidth = 1.9 * holeRadius;
+        this.context.beginPath();
+        this.context.moveTo(xMiddle, yTop);
+        this.context.lineTo(xMiddle, yTop - holeRadius);
+        this.context.stroke();
+
+        this.context.fillStyle = "black";
+        this.context.beginPath();
+        this.context.arc(xMiddle, yTop - holeRadius, holeRadius, 0, 2 * Math.PI);
+        this.context.fill();
+
+        // top right hole
+        this.context.strokeStyle = Table.COLOR;
+        this.context.lineCap = "round";
+        this.context.lineWidth = 1.9 * holeRadius;
+        this.context.beginPath();
+        this.context.moveTo(xRight - holeOffset, yTop + holeOffset);
+        this.context.lineTo(xRight + holeOffset, yTop - holeOffset);
+        this.context.stroke();
+
+        this.context.fillStyle = "black";
+        this.context.beginPath();
+        this.context.arc(xRight + holeOffset, yTop - holeOffset, holeRadius, 0, 2 * Math.PI);
+        this.context.fill();
+
+        // bottom right hole
+        this.context.strokeStyle = Table.COLOR;
+        this.context.lineCap = "round";
+        this.context.lineWidth = 1.9 * holeRadius;
+        this.context.beginPath();
+        this.context.moveTo(xRight - holeOffset, yBottom - holeOffset);
+        this.context.lineTo(xRight + holeOffset, yBottom + holeOffset);
+        this.context.stroke();
+
+        this.context.fillStyle = "black";
+        this.context.beginPath();
+        this.context.arc(xRight + holeOffset, yBottom + holeOffset, holeRadius, 0, 2 * Math.PI);
+        this.context.fill();
+
+        // bottom middle hole
+        this.context.strokeStyle = Table.COLOR;
+        this.context.lineCap = "round";
+        this.context.lineWidth = 1.9 * holeRadius;
+        this.context.beginPath();
+        this.context.moveTo(xMiddle, yBottom);
+        this.context.lineTo(xMiddle, yBottom + holeRadius);
+        this.context.stroke();
+
+        this.context.fillStyle = "black";
+        this.context.beginPath();
+        this.context.arc(xMiddle, yBottom + holeRadius, holeRadius, 0, 2 * Math.PI);
+        this.context.fill();
+
+        // bottom left hole
+        this.context.strokeStyle = Table.COLOR;
+        this.context.lineCap = "round";
+        this.context.lineWidth = 1.9 * holeRadius;
+        this.context.beginPath();
+        this.context.moveTo(xLeft + holeOffset, yBottom - holeOffset);
+        this.context.lineTo(xLeft - holeOffset, yBottom + holeOffset);
+        this.context.stroke();
+
+        this.context.fillStyle = "black";
+        this.context.beginPath();
+        this.context.arc(xLeft - holeOffset, yBottom + holeOffset, holeRadius, 0, 2 * Math.PI);
         this.context.fill();
     }
 }
 
 Table.WIDTH = scaleRealCentimetersToPixel(270);
 Table.HEIGHT = scaleRealCentimetersToPixel(270 / 2);
+Table.COLOR = "green";
+Table.BORDER_OUTER_COLOR = "#432918";
+Table.BORDER_INNER_COLOR = "#7d4e24";
+Table.BORDER_WIDTH = 20;
+
 
 
 class Ball {

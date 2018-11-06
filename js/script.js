92600494426e012e0618df3647edc065dfa4b788
Jendrik <buhtig@jendrik.eu>
2018-11-06 09:34:28 +0100

    remove duplicate function

--- js/script.js
+++ js/script.js
@@ -1,10 +1,5 @@
 'use strict';
 
-// converts a dimension of an official pool table to pixel for the canvas
-function scaleRealCentimetersToPixel(cm) {
-    return cm * 2.2;
-}
-
 function onload() {
     Cue.IMG = document.getElementById("cue-img");
     Cue.WIDTH = Cue.IMG.naturalHeight * (Cue.LENGTH / Cue.IMG.naturalWidth);
@@ -12,12 +7,3 @@ function onload() {
     let game = new Game();
     game.drawAll();
 }
-
-
-
-/*
-//rotaiting pictures on website
-function rotateAnimation(img, degrees){
-	img.style.MozTransform = "rotate("+degrees+"deg)";
-}
-*/

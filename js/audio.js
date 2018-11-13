0b5f39f222a9932575ece5ded0a472175536c4a2
4sakura <4sakura@web.de>
2018-11-13 10:47:10 +0100

    switched the music-control text with an icon

--- js/audio.js
+++ js/audio.js
@@ -11,9 +11,20 @@ function toggleMusic() {
 }
 
 function toggleText(button) {
-    if (button.innerText === "Musik aus") {
-        button.innerText = "Musik an";
-    } else {
-        button.innerText = "Musik aus";
+	if (button.innerText === "Musik aus") {
+		button.innerText = "Musik an";
+	} else {
+		button.innerText = "Musik aus";
+	}
+}
+
+function toggleClass() {
+    const cm = document.getElementById('control-music');
+    if (cm.className === "mute"){
+        cm.className = "unmute";
+    }
+    else {
+        cm.className = "mute";
     }
 }
+

fa2ca36749b8868ab19275336681a2ba5c97a7c9
Stephan <Stephan.Toennies@gmail.com>
2018-12-18 11:08:43 +0100

    fix JSON

--- js/score.js
+++ js/score.js
@@ -1,21 +1,10 @@
 'use strict';
-/*
-let myRequest = new Request('score.json');
 
-fetch(myRequest)
-.then(function(response) {
-  if (!response.ok) {
-    throw new Error('HTTP error, status = ' + response.status);
-  }
-  return response.blob();
-})
-.then(function(response) {
-  console.log(response);
-});
-*/
-var json;
-fetch('../score.json')
+fetch('score.json')
   .then(response => response.json())
-  .then(jsonResponse => json = jsonResponse);
+  .then(json = asynchronJson(json));
 
-  var obj = JSON.parse(json);
+  
+ function asynchronJson(json){
+	console.log(json);
+ }

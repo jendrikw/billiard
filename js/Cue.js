a6f106f2d82cb3193c9f88520e2fca7c2e4beb9f
Jendrik <buhtig@jendrik.eu>
2019-01-20 23:19:04 +0100

    add some comments

--- js/Cue.js
+++ js/Cue.js
@@ -3,6 +3,9 @@
 class Cue {
 
     constructor(game, whiteBall) {
+        Cue.IMG = document.getElementById("cue-img");
+        Cue.WIDTH = Cue.IMG.naturalHeight * (Cue.LENGTH / Cue.IMG.naturalWidth);
+
         this.game = game;
         this.whiteBall = whiteBall;
         this.context = getContext("cue-canvas");
@@ -10,10 +13,7 @@ class Cue {
         this.power = 0;
         this.distance = 0;
         this.mouseDown = false;
-        this.mouseClientX = null; // relative to the page
-        this.mouseClientY = null;
         this.theta = null;
-        this.bumpCounter = 0;
         this.context.canvas.addEventListener("mousemove", this.internalMouseMove = e => this.onCanvasMouseMove(e));
         this.context.canvas.addEventListener("mousedown", this.internalMouseDown = () => this.onCanvasMouseDown());
         this.context.canvas.addEventListener("mouseup", this.internalMouseUp = () => this.onCanvasMouseUp());
@@ -21,19 +21,27 @@ class Cue {
     }
 
     onCanvasMouseMove(event) {
-        this.mouseClientX = event.clientX;
-        this.mouseClientY = event.clientY;
+        // this method changes the angle of the cue
+        // it is not allowed to change if the mouse is being held down,
+        // the cue is already shooting or allowedToShoot returns false
         if (!this.mouseDown && !this.power && this.allowedToShoot()) {
+            // calculate the coordinates of the mouse relative to the canvas
             const mouseCanvasX = event.clientX - this.canvas.offsetLeft;
             const mouseCanvasY = event.clientY - this.canvas.offsetTop;
             // move cue around the ball
             this.theta = Math.atan((mouseCanvasY - this.whiteBall.y) / (mouseCanvasX - this.whiteBall.x));
+
+            // atan returns a value between -pi/2 and pi, which is only half of a circle
+            // test if the cue is on the other side of the angle
             if (mouseCanvasX - this.whiteBall.x < 0) {
                 this.theta += Math.PI;
             }
 
+            // redraw
             this.clear();
             this.context.setTransform(1, 0, 0, 1, 0, 0);
+            // important: rotate the canvas by the calculated angle around the white ball,
+            // because it is impossible to draw rotated images
             this.context.translate(this.whiteBall.x, this.whiteBall.y);
             this.context.rotate(this.theta);
             this.drawWithDistance(Cue.BALL_CUE_DISTANCE);
@@ -42,7 +50,7 @@ class Cue {
 
     onWindowMouseMove(event) {
         if (event.target !== this.context.canvas) {
-            // mouse outside of canvas
+            // if the mouse is outside of canvas, stop movement
             this.distance = 0;
             this.mouseDown = false;
         }
@@ -54,9 +62,11 @@ class Cue {
         }
         this.mouseDown = true;
         this.distance = 0;
+        // start increasing the distance
         this.increaseDistance();
     }
 
+    // increase the distance while the mouse is pressed
     increaseDistance() {
         this.distance += 0.5;
         this.clear();
@@ -68,15 +78,18 @@ class Cue {
         }
     }
 
+    // shoot when the mouse is released
     onCanvasMouseUp() {
         if (!this.allowedToShoot()) {
             return;
         }
         this.mouseDown = false;
         this.power = Math.min(this.distance * Cue.CUE_POWER_TO_BALL_SPEED_FACTOR, 1.5 * Math.sqrt(this.distance));
+        // start the shooting process
         this.shoot();
     }
 
+    // shooting means the distance is decreasing automatically until the cue hits the white ball
     shoot() {
         this.distance -= 5;
         if (this.distance < 0) {
@@ -90,6 +103,7 @@ class Cue {
             this.whiteBall.bump(this.theta, this.power);
             this.power = null;
         } else {
+            // call shoot again
             this.clear();
             this.drawWithDistance(this.distance + Ball.RADIUS);
             window.requestAnimationFrame(() => this.shoot());
@@ -110,12 +124,14 @@ class Cue {
     }
 
     allowedToShoot() {
+        // the player is not allowed to shoot if some balls are still moving or the game is paused
         const b = !this.game.areAnyBallsMoving() && !this.game.isPaused;
         console.log("allowedToShoot", b);
         return b;
     }
 
     kill() {
+        // when a new game is started, this cue shouldn't do anything anymore
     	delete this.game;
     	delete this.canvas;
     	this.context.canvas.removeEventListener("mousemove", this.internalMouseMove);
@@ -128,6 +144,6 @@ class Cue {
 
 Cue.LENGTH = 150;
 Cue.BALL_CUE_DISTANCE = 20;
-Cue.IMG = null; // set in onload
-Cue.WIDTH = null; // set in onload
+Cue.IMG = null; // set in constructor
+Cue.WIDTH = null; // set in constructor
 Cue.CUE_POWER_TO_BALL_SPEED_FACTOR = 0.3;
--- js/script.js
+++ js/script.js
@@ -1,13 +1,6 @@
 'use strict';
 
 function onload() {
-	try{
-		Cue.IMG = document.getElementById("cue-img");
-	    Cue.WIDTH = Cue.IMG.naturalHeight * (Cue.LENGTH / Cue.IMG.naturalWidth);
-	} catch(err) {
-		console.log("Es gab einen Fehler beim Cue. " + err);
-	}
-
     let game = new Game();
     game.start();
 }

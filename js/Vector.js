e3cba1ab67b6d11d1139b136e70e398ea3f719cd
4sakura <4sakura@web.de>
2019-01-20 23:17:17 +0100

    added coments

--- js/Vector.js
+++ js/Vector.js
@@ -7,41 +7,36 @@ class Vector {
         this.y = y;
     }
 
+    //normalises a vector
     normalize() {
         const length = this.length();
         this.x /= length;
         this.y /= length;
     }
 
+    //returns the length of a vector
     length() {
         return Math.sqrt(this.x * this.x + this.y * this.y);
     }
 
+    //scales a vector by a factor
     scale(factor) {
         this.x *= factor;
         this.y *= factor;
         return this;
     }
 
-    setLength(length) {
-        this.normalize();
-        // length is now 1
-        this.scale(length);
-        return this;
-    }
-
+    //adds two vectors and returns the resulting vector
     plus(that) {
         return new Vector(this.x + that.x, this.y + that.y);
     }
 
-    minus(that) {
-        return new Vector(this.x - that.x, this.y - that.y);
-    }
-
+    //returns the dotproduct of two vectors
     dotProduct(that) {
         return (this.x * that.x) + (this.y * that.y);
     }
 
+    //returns a copy of an vector
     copy() {
         return new Vector(this.x, this.y);
     }
